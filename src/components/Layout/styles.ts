2432db96d63ba93c122ba717d995f9cc22b9a25c
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-22 23:54:24 -0300

    change grid column size in layout component

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.main`
   display: grid;
-  grid-template-columns: 10rem calc(100vw - 35rem) 25rem;
+  grid-template-columns: 10rem calc(100vw - 35rem) auto;
   grid-template-areas: 'actions content playing';
-  width: 100vw;
+  width: 100%;
   height: calc(100vh - 5rem);
 `
