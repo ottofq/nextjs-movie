4e135f3d08f49f6e2409e05f241be8eea774a49b
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-22 23:44:26 -0300

    refactor movie card

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,17 +1,57 @@
 import React from 'react'
+import Link from 'next/link'
+import { AiFillStar } from 'react-icons/ai'
 
 import * as S from './styles'
+import movieGenres from './movieGenres'
 
-const MovieCard = () => {
+export interface MovieProps {
+  id: string
+  title: string
+  name?: string
+  genre_ids: number[]
+  vote_average?: number
+  backdrop_path: string
+}
+
+const MovieCard = ({
+  id,
+  title,
+  name,
+  genre_ids,
+  vote_average,
+  backdrop_path
+}: MovieProps) => {
+  const sliceGenres = genre_ids.slice(0, 3)
+
+  const genreNames = movieGenres.filter(item => sliceGenres.includes(item.id))
+
   return (
-    <S.Container>
-      <S.Image src="https://image.tmdb.org/t/p/original/zzWGRw277MNoCs3zhyG3YmYQsXv.jpg" />
-      <S.MovieName>Mulan</S.MovieName>
-      <S.Genres>
-        <p>Drama</p>
-        <span>7.5/10</span>
-      </S.Genres>
-    </S.Container>
+    <Link
+      href={`/movie/${id}/${
+        name?.replace(/\s/g, '-') || title?.replace(/\s/g, '-')
+      }`}
+    >
+      <a>
+        <S.Container>
+          <S.Image
+            src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+          />
+          <S.MovieName>{name || title}</S.MovieName>
+          <S.Genres>
+            <p>{genreNames.map(item => item.name).join(', ')}</p>
+            {vote_average ? (
+              <>
+                <AiFillStar size={15} />
+                <span>{vote_average}/10</span>
+              </>
+            ) : (
+              ''
+            )}
+          </S.Genres>
+        </S.Container>
+      </a>
+    </Link>
   )
 }
 
--- src/components/MovieCard/styles.ts
+++ src/components/MovieCard/styles.ts
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 0.5rem;
   height: 20rem;
-  width: 25rem;
+  max-width: 25rem;
 `
 
 export const Image = styled.img`
@@ -13,6 +13,24 @@ export const Image = styled.img`
   width: 100%;
 `
 
-export const MovieName = styled.p``
+export const MovieName = styled.p`
+  font-size: 1.6rem;
+  font-weight: bold;
+  color: #ccc;
+`
 
-export const Genres = styled.div``
+export const Genres = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #888;
+
+  p,
+  span {
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+  svg {
+    margin: 0 0.3rem 0 auto;
+  }
+`
