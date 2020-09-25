7bb80f9f17576ca6e3f62aec1edfc5094acba08e
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-24 22:37:27 -0300

    refactor movie details component

--- src/components/MovieDetails/index.tsx
+++ src/components/MovieDetails/index.tsx
@@ -1,58 +1,94 @@
 import React, { useState } from 'react'
+import { format, parseISO } from 'date-fns'
 import { AiFillStar } from 'react-icons/ai'
 import { BsHeartFill, BsFillEyeFill } from 'react-icons/bs'
 import { FaPlay } from 'react-icons/fa'
 
 import Button from '../Button'
 import TrailerModal from '../TrailerModal'
+import { MovieProps } from '../MovieCard'
 
 import * as S from './styles'
 
-const MovieDetails = () => {
+export interface TrailerProps {
+  id: number
+  key: string
+  site: number
+}
+
+export interface CrewProps {
+  id: number
+  name: string
+  job: string
+}
+
+interface Props {
+  movie: MovieProps
+  crews: CrewProps[]
+  trailer: TrailerProps
+}
+
+const MovieDetails = ({ movie, crews, trailer }: Props) => {
   const [showModal, setShowModal] = useState(false)
+
+  function formatDate(date) {
+    const parseDate = parseISO(movie.release_date)
+    return format(parseDate, 'dd/MM/yyyy')
+  }
+
+  function timeConvert(num) {
+    var hours = Math.floor(num / 60)
+    var minutes = num % 60
+    return `${hours}h ${minutes}m`
+  }
+
+  function closeModal() {
+    setShowModal(false)
+  }
+
+  function openModal() {
+    setShowModal(true)
+  }
 
   return (
     <S.Container>
-      <S.PosterImage src="https://image.tmdb.org/t/p/w342/aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg" />
+      <S.PosterImage
+        src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+      />
       <S.ContentContainer>
         <S.TitleContainer>
-          <S.Title>Mulan</S.Title>
+          <S.Title>{movie.title}</S.Title>
           <>
             <AiFillStar size={24} />
-            <span>7.4/10</span>
+            <span>{movie.vote_average}/10</span>
           </>
         </S.TitleContainer>
-        <S.Details>
-          When the Emperor of China issues a decree that one man per family must
-          serve in the Imperial Chinese Army to defend the country from Huns,
-          Hua Mulan, the eldest daughter of an honored warrior, steps in to take
-          the place of her ailing father. She is spirited, determined and quick
-          on her feet. Disguised as a man by the name of Hua Jun, she is tested
-          every step of the way and must harness her innermost strength and
-          embrace her true potential.
-        </S.Details>
-        <S.TitleActors>Actors</S.TitleActors>
-        <S.ActorsNameContainer>
-          <S.ActorName>Liu Yifei</S.ActorName>
-          <S.ActorName>Liu Yifei</S.ActorName>
-          <S.ActorName>Liu Yifei</S.ActorName>
-          <S.ActorName>Liu Yifei</S.ActorName>
-          <S.ActorName>Liu Yifei</S.ActorName>
-          <S.ActorName>Liu Yifei</S.ActorName>
-          <S.ActorName>Liu Yifei</S.ActorName>
-        </S.ActorsNameContainer>
+        <S.Facts>
+          <span>{formatDate(movie.release_date)}</span>
+          <span> {movie.genres.map(genre => genre.name).join(', ')}</span>
+          <span>{timeConvert(movie.runtime)}</span>
+        </S.Facts>
+        <S.Details>{movie.overview}</S.Details>
+        <S.TitleActors>Crew</S.TitleActors>
+        <S.CrewsNameContainer>
+          {crews.map(crew => (
+            <li key={crew.id}>
+              <p>{crew.name}</p>
+              <p>{crew.job}</p>
+            </li>
+          ))}
+        </S.CrewsNameContainer>
         <S.ActionsContainer>
-          <Button
-            Icon={<FaPlay size={20} />}
-            onClick={() => setShowModal(true)}
-          >
+          <Button Icon={<FaPlay size={20} />} onClick={() => openModal()}>
             Watch Trailer
           </Button>
           <Button Icon={<BsHeartFill size={20} />}>Fav</Button>
           <Button Icon={<BsFillEyeFill size={20} />}>Add Watch List</Button>
         </S.ActionsContainer>
       </S.ContentContainer>
-      <TrailerModal setShow={setShowModal} show={showModal} />
+      {showModal && (
+        <TrailerModal youtubeId={trailer.key} closeModal={closeModal} />
+      )}
     </S.Container>
   )
 }
--- src/components/MovieDetails/styles.ts
+++ src/components/MovieDetails/styles.ts
@@ -4,12 +4,14 @@ export const Container = styled.section`
   display: grid;
   grid-template-columns: 32.4rem auto;
   padding: 0 1rem;
-  height: 40rem;
+  height: 100%;
+  width: 100%;
 `
 
 export const PosterImage = styled.img`
   height: 100%;
   width: 100%;
+  border-radius: 0.5rem;
 `
 
 export const ContentContainer = styled.div`
@@ -17,7 +19,7 @@ export const ContentContainer = styled.div`
   padding: 0 2rem;
   flex-direction: column;
   flex: 1;
-  gap: 1rem;
+  gap: 0.8rem;
 `
 
 export const TitleContainer = styled.div`
@@ -40,6 +42,27 @@ export const Title = styled.h1`
   color: #f0ece2;
 `
 
+export const Facts = styled.div`
+  width: 100%;
+  position: relative;
+
+  span {
+    & + span {
+      &::before {
+        content: '';
+        display: inline-flex;
+        position: relative;
+        top: -0.3rem;
+        width: 0.5rem;
+        height: 0.5rem;
+        background-color: white;
+        border-radius: 50%;
+        margin: 0 0.8rem;
+      }
+    }
+  }
+`
+
 export const Details = styled.p`
   font-size: 1.8rem;
   line-height: 3.5rem;
@@ -52,10 +75,20 @@ export const TitleActors = styled.h2`
   color: #f0ece2;
 `
 
-export const ActorsNameContainer = styled.div`
+export const CrewsNameContainer = styled.ul`
   display: flex;
-  gap: 1rem;
+  flex-wrap: wrap;
   align-items: center;
+  width: 100%;
+
+  li {
+    width: 33%;
+    margin-bottom: 1rem;
+    p:first-child {
+      font-size: 1.6rem;
+      font-weight: bold;
+    }
+  }
 `
 
 export const ActorName = styled.p`
