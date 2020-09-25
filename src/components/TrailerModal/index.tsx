6b9ea7ade1d54d65ebedb233e96c81e3075c54c6
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-24 22:41:03 -0300

    add trailer modal

---
+++ src/components/TrailerModal/index.tsx
@@ -0,0 +1,52 @@
+import React, { memo, useRef, useEffect, useCallback } from 'react'
+
+import * as S from './styles'
+
+interface Props {
+  closeModal: Function
+  youtubeId: string
+}
+
+const TrailerModal = ({ closeModal, youtubeId }: Props) => {
+  const ref = useRef(null)
+
+  const escapeListener = useCallback((e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      closeModal()
+    }
+  }, [])
+
+  const clickListener = useCallback(
+    (e: MouseEvent) => {
+      if (!(ref.current! as any).contains(e.target)) {
+        closeModal()
+      }
+    },
+    [ref.current]
+  )
+
+  useEffect(() => {
+    document.addEventListener('click', clickListener)
+    document.addEventListener('keydown', escapeListener)
+
+    return () => {
+      document.removeEventListener('click', clickListener)
+      document.removeEventListener('keydown', escapeListener)
+    }
+  })
+
+  return (
+    <S.Container>
+      <S.Content ref={ref}>
+        <iframe
+          width="1080"
+          height="600"
+          allowFullScreen
+          src={`https://www.youtube.com/embed/${youtubeId}`}
+        ></iframe>
+      </S.Content>
+    </S.Container>
+  )
+}
+
+export default memo(TrailerModal)
---
+++ src/components/TrailerModal/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100vh;
+  width: 100vw;
+  z-index: 100;
+  background-color: rgba(1, 1, 1, 0.9);
+`
+
+export const Content = styled.div`
+  /* border: 1px solid #ccc; */
+`
