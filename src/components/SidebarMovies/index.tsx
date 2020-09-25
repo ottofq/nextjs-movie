591fbe4d941f279de8c73742d21e67dcb4c9dbd1
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-24 22:38:16 -0300

    refactor sidebar movies

--- src/components/SidebarMovies/index.tsx
+++ src/components/SidebarMovies/index.tsx
@@ -1,28 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
-import MovieCard from '../MovieCard'
+import MovieCard, { MovieProps } from '../MovieCard'
 
 import * as S from './styles'
 import axios from 'axios'
 import useSWR from 'swr'
 
-export interface Movie {
-  title: string
-  id: number
-}
-
 export interface Props {
-  movies: Movie[]
+  movies: MovieProps[]
 }
 
 const SidebarMovies = () => {
-  const { data: response, error } = useSWR('/api/movies', axios)
+  const { data: response, error } = useSWR('/api/movies', axios, {
+    refreshInterval: 0
+  })
 
   if (error) return <div>failed to load</div>
   if (!response) return <div>loading...</div>
 
   const movies = response.data.movies
-  console.log(response.data.movies)
 
   return (
     <S.Container>
@@ -30,19 +26,7 @@ const SidebarMovies = () => {
       <ul>
         {movies.map(movie => (
           <li key={movie.id}>
-            <Link
-              key={movie.id}
-              href={`/movie/${movie.id}/${movie.title.replace(/\s/g, '-')}`}
-            >
-              <a>
-                <MovieCard
-                  title={movie.title}
-                  backdrop_path={movie.backdrop_path}
-                  genre_ids={movie.genre_ids || []}
-                  id={movie.id}
-                />
-              </a>
-            </Link>
+            <MovieCard movie={movie} />
           </li>
         ))}
       </ul>
