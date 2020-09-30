ac8126afc437af1dd6667dd6e117aa87f26f5b68
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-29 22:31:31 -0300

    add styles in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import { BsHeartFill, BsFillEyeFill } from 'react-icons/bs'
 
 import * as S from './styles'
+import Logo from '../../assets/movie.svg'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <S.Container>
-      <p>header</p>
+      <S.LogoContainer>
+        <Link href="/">
+          <a>
+            <Logo />
+            <h1>Next Movies</h1>
+          </a>
+        </Link>
+      </S.LogoContainer>
+
+      <S.ActionsContainer>
+        <ul>
+          <li>
+            <Link href="/favorites">
+              <a href="">
+                <BsHeartFill size={25} />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/watchlist">
+              <a href="">
+                <BsFillEyeFill size={25} />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </S.ActionsContainer>
     </S.Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,51 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  grid-area: header;
   width: 100%;
-  height: 5rem;
+`
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 16rem;
+
+  a {
+    display: flex;
+    align-items: center;
+  }
+
+  h1 {
+    font-size: 2.2rem;
+    font-weight: bold;
+    color: #f0ece2;
+  }
+
+  svg {
+    height: 100%;
+    width: 3rem;
+    color: #f0ece2;
+    margin-right: 0.5rem;
+  }
+`
+
+export const ActionsContainer = styled.nav`
+  ul {
+    display: flex;
+    height: 100%;
+    width: 10rem;
+    color: #f0ece2;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+
+    li {
+      a {
+        color: inherit;
+      }
+    }
+  }
 `
