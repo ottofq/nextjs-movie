ed99244935a06488d512b9ac19682def0e200350
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-29 22:31:59 -0300

    add footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { AiFillGithub } from 'react-icons/ai'
+
+import * as S from './styles'
+
+const Footer = () => {
+  return (
+    <S.Container>
+      <a
+        href="https://github.com/ottofq/nextjs-movie"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <AiFillGithub size={40} />
+      </a>
+    </S.Container>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.footer`
+  display: flex;
+  padding-bottom: 1rem;
+  margin-top: auto;
+  align-items: center;
+  justify-content: center;
+  grid-area: footer;
+`
