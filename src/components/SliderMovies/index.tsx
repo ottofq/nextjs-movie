8ee7c1fea2fa1d766f1d8bb32e05bbf0c581099d
Otto F. Quintanilha <ottofq@gmail.com>
2020-10-01 23:47:45 -0300

    add link in movies slider

--- src/components/SliderMovies/index.tsx
+++ src/components/SliderMovies/index.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link'
 import React from 'react'
+import slugify from 'slugify'
 
 import MovieCard, { MovieProps } from '../MovieCard'
 import Slider from '../Slider'
@@ -14,7 +16,14 @@ const MovieSlider = ({ movies, title }: Props) => {
   return (
     <Slider title={title}>
       {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
+        <Link
+          key={movie.id}
+          href={`/movie/${movie.id}/${slugify(movie.title)}`}
+        >
+          <a>
+            <MovieCard movie={movie} />
+          </a>
+        </Link>
       ))}
     </Slider>
   )
