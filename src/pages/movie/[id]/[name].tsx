9dfa1224b2bf9d007dce3f065bfe36d1a8d4f61c
Otto F. Quintanilha <ottofq@gmail.com>
2020-10-01 23:50:13 -0300

    add more components in movie page

--- src/pages/movie/[id]/[name].tsx
+++ src/pages/movie/[id]/[name].tsx
@@ -1,37 +1,76 @@
 import React from 'react'
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 
-import MainContent from '../../../components/MainContent'
+import MovieDetails, {
+  CrewProps,
+  TrailerProps
+} from '../../../components/MovieDetails'
+import { MovieProps } from '../../../components/MovieCard'
+import SliderPeople, { PeopleProps } from '../../../components/SliderPeople'
+import SliderMovies from '../../../components/SliderMovies'
+
 import api from '../../../services/api'
-
-import * as S from '../../../styles/pages/Home'
+import * as S from '../../../styles/pages/Movie'
 
 interface Props {
-  data: string
+  movie: MovieProps
+  similar: MovieProps[]
+  crews: CrewProps[]
+  trailer: TrailerProps[]
+  cast: PeopleProps[]
 }
 
-export const getServerSideProps = async ({ req, params }) => {
-  const id = params.id
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { id } = params
 
-  const response = await api.get(`/movie/${id}`)
+  const movieDataResponse = await api.get(`/movie/${id}`)
+  const videoDataResponse = await api.get(`/movie/${id}/videos`)
+  const creditsDataResponse = await api.get(`/movie/${id}/credits`)
+  const similarMovieResponse = await api.get(`/movie/${id}/similar`)
 
-  const data = response.data
+  const crewFilter = ['Directing', 'Writing']
+
+  const movie = movieDataResponse.data
+  const video = videoDataResponse.data.results
+  const cast = creditsDataResponse.data.cast
+  const crews = creditsDataResponse.data.crew.filter(
+    item => crewFilter.includes(item.department) && item.job !== 'Layout'
+  )
+  const similar = similarMovieResponse.data.results
 
   return {
     props: {
-      data
+      movie,
+      similar,
+      trailer: video,
+      cast: cast.slice(0, 16),
+      crews
     }
   }
 }
 
-const Movie = ({ data }: Props) => {
+const Movie: React.FC<Props> = ({ movie, similar, trailer, cast, crews }) => {
   return (
     <>
       <Head>
-        <title>Movies | Nome do Filme </title>
+        <title>{`Next Movies | ${movie.title} `}</title>
       </Head>
       <S.Container>
-        <p>{JSON.stringify(data)}</p>
+        <S.Wrapper>
+          <S.MovieInfo>
+            <MovieDetails
+              movie={movie}
+              trailer={trailer.length > 0 ? trailer[0] : null}
+              crews={crews}
+            />
+          </S.MovieInfo>
+
+          <S.Movies>
+            <SliderPeople title="Cast" peoples={cast} />
+            <SliderMovies title="Similar Movies" movies={similar} />
+          </S.Movies>
+        </S.Wrapper>
       </S.Container>
     </>
   )
---
+++ src/styles/pages/Movie.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+`
+
+export const Wrapper = styled.div`
+  height: 100%;
+`
+
+export const MovieInfo = styled.section`
+  min-height: 40rem;
+`
+
+export const Movies = styled.section``
